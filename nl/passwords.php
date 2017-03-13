d9ae4e1d30c3118c8ba3f612899d2415af4b7776
surudoiryu <nigelbloemendal@gmail.com>
2017-03-13 22:49:45 +0100

    Added language NL files

---
+++ nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Wachtwoorden moeten minimaal zes tekens zijn, en overeenkomen.',
+    'reset' => 'Uw wachtwoord is gereset!',
+    'sent' => 'We hebben u een e-mail met een reset link voor uw wachtwoord verstuurd.',
+    'token' => 'De opgegeven reset token voor uw wachtwoord is ongeldig.',
+    'user' => "We kunnen geen gebruiker vinden met het opgegeven e-mailadres.",
+
+];
